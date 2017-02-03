50811761b3f2f9a9448c296dfaa1d9efdc194c9e
Sam Davies <sdjdavies42@gmail.com>
2017-02-03 20:50:24 +0100

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,20 @@ class ProfilesController < ApplicationController
 	def new
 		@profile = Profile.new
 	end
+	# POST request to /users/:user_id/profile
+	def create
+		@user = User.find( params[:user_id] )
+		@profile = @user.build_profile( profile_params )
+		if @profile.save
+			flash[:success] = "You've created your dealership profile. Good selling!"
+			redirect_to root_path
+		else
+			render action: :new
+		end
+	end
+
+	private
+		def profile_params
+			params.require(:profile).permit(:company, :name, :location, :phone_number, :email, :about)
+		end
 end
