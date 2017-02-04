fee9d7de5bf4b90f230e189c6b7240cd1fc28b94
Sam Davies <sdjdavies42@gmail.com>
2017-02-04 22:28:49 +0100

    Added update action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,24 @@ class ProfilesController < ApplicationController
 		end
 	end
 
+	# GET request to /users/:user_id/profile/edit
+	def edit
+		@user = User.find( params[:user_id] )
+		@profile = @user.profile
+	end
+
+	# PUT request to /users/:user_id/profile
+	def update
+		@user = User.find( params[:user_id] )
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Page Updated"
+			redirect_to user_path(id: params[:user_id] )
+		else
+			render action: :edit
+		end
+	end
+
 	private
 		def profile_params
 			params.require(:profile).permit(:avatar, :company, :name, :location, :phone_number, :email, :about)
