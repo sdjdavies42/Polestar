2ef6bac71ba42ca4ad73021077d8bb8f302983e0
Sam Davies <sdjdavies42@gmail.com>
2017-02-07 19:28:30 +0100

    Added cars create action

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,4 +5,19 @@ class CarsController < ApplicationController
 		@car = Car.new
 	end
 
+	# POST request to /users/:user_id/cars
+	def create
+		# Finds user who is filling out form
+		@user = User.find( params[:user_id] )
+		# Create car linked to this specific user
+		@car = @user.build_car( car_params )
+		if @car.save 
+			redirect_to user_profile_path
+		end
+	end
+
+	private
+		def car_params
+			params.require(:car).permit(:year, :make, :model, :miles, :transmission, :description)
+		end
 end
