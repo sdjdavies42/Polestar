f1fcccd8f900b9762d3de8bcf01df61ac64ea10a
Sam Davies <sdjdavies42@gmail.com>
2017-02-20 18:20:44 +0100

    Fixed contact mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,44 @@
 class ContactsController < ApplicationController
-	def new
-		@contact = Contact.new
-	end
-
-	def create
-		@contact = Contact.new(contact_params)
-		if @contact.save
-			name = params[:contact][:name]
-			email = params[:contact][:email]
-			phone = params[:contact][:phone]
-			body = params[:contact][:message]
-			ContactMailer.contact_email(name, email, body).deliver
-			flash[:success] = "Message sent. Someone will be in touch with you soon. Thanks!"
-			redirect_to new_contact_path
-		else 
-			flash[:danger] = @contact.errors.full_messages.join(", ")
-			redirect_to new_contact_path
-		end
-	end
-
-	private
-		def contact_params
-			params.require(:contact).permit(:name, :email, :phone, :message)		
-		end
-end
+   
+   #GET request to /contact-us
+   #Show new contact form
+   def new
+      @contact = Contact.new
+   end
+   
+   #POST request /contacts
+   def create
+    #Mass assignment of form fields into Contact object
+    @contact = Contact.new(contact_params)
+    #Save the Contact object to the database
+      if @contact.save
+        #Store form fields via parameters into variables
+          name = params[:contact][:name]
+          phone = params[:contact][:phone]
+          email = params[:contact][:email]
+          body = params[:contact][:message]
+        #Plug variables into the contact mailer email method 
+        # and send email
+          ContactMailer.contact_email(name, phone, email, body).deliver
+        #Store success message in flash hash
+        # and redirect to the new action
+          flash[:success] = "Message sent."
+          redirect_to new_contact_path
+      else
+          #If contact option doesn't save,
+          # store errors in flash hash and
+          # redirect to the new action
+          flash[:danger] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
+      end
+   end
+   
+    private
+    #To collect data from form, use 
+    # strong parameters and whitelist
+    # the form fields
+      def contact_params
+         params.require(:contact).permit(:name, :email, :phone, :message)
+     end
+       
+end 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
 	default to: 'sdjdavies42@gmail.com'
 
-	def contact_email(name, email, phone, body)
+	def contact_email(name, phone, email, message)
 		@name = name
 		@email = email
 		@phone = phone
-		@body = body
+		@body = message
 		mail(from: email, subject: 'Contact Form message')
 	end	
 end
