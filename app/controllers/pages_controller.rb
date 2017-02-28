1f15ee235687bae2bd8cd057570c7e19a3209c62
Sam Davies <sdjdavies42@gmail.com>
2017-02-28 23:06:22 +0100

    Added preview pictures to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
 	
 	def home
 		@dealer_plan = Plan.find(1)
+		@car1 = Car.last
+		@car2 = Car.last(2).first
+		@car3 = Car.last(3).first
+		@car4 = Car.last(4).first
 	end
 
 	def about
