49f8753edbc223a419c488389dbe1486ec460010
Sam Davies <sdjdavies42@gmail.com>
2017-03-12 11:19:57 +0100

    Fixed search by category

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,7 +8,7 @@ class Search < ApplicationRecord
 		cars = cars.where(["model LIKE ?", "%#{model}%"]) if model.present?
 		cars = cars.where(["description LIKE ?", "%#{keywords}%"]) if keywords.present?
 		cars = cars.where(["transmission LIKE ?", transmission]) if transmission.present?
-		cars = cars.where(["transmission LIKE ?", category]) if category.present?
+		cars = cars.where(["category LIKE ?", category]) if category.present?
 
 		return cars
 
